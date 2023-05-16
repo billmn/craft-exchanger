220ee3d56bec1f70f3145f4f69325929e6ab02bc
Davide Bellini <bellini.davide@gmail.com>
2023-05-17 00:00:35 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Exchanger
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace billmn\exchanger\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Exchanger settings
+ */
+class Settings extends Model
+{
+}
